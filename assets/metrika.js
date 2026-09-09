cda20134b5dba0b323ee113d5dc0f7b9837ef3a9
olroumiantseva-lab <olroumiantseva@gmail.com>
2026-09-09 14:22:03 +0100

    Add Yandex Metrika analytics helper

---
+++ assets/metrika.js
@@ -0,0 +1,29 @@
+(() => {
+  'use strict';
+  const counterId = 112426595;
+  (function(m,e,t,r,i,k,a){
+    m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+    m[i].l=1*new Date();
+    for (let j=0;j<document.scripts.length;j+=1){if(document.scripts[j].src===r)return;}
+    k=e.createElement(t);a=e.getElementsByTagName(t)[0];k.async=1;k.src=r;a.parentNode.insertBefore(k,a);
+  })(window,document,'script',`https://mc.yandex.ru/metrika/tag.js?id=${counterId}`,'ym');
+
+  window.ym(counterId,'init',{
+    ssr:true,
+    webvisor:true,
+    clickmap:true,
+    ecommerce:'dataLayer',
+    referrer:document.referrer,
+    url:location.href,
+    accurateTrackBounce:true,
+    trackLinks:true,
+  });
+
+  window.proverjdoGoal=(name,params)=>{
+    try{window.ym(counterId,'reachGoal',name,params||{});}catch{}
+  };
+
+  const path=location.pathname.replace(/\/+$/,'/') || '/';
+  if(path==='/') window.proverjdoGoal('landing_view');
+  if(path==='/check/') window.proverjdoGoal('check_start');
+})();
